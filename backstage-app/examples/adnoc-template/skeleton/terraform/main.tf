c4710e308decee3331a029069214ebf9105056b2
Mostafa <ms.elkhouly97@gmail.com>
2026-07-16 11:41:36 +0300

    fix: update Azure App Service deployment workflow for Node.js adnoc test app

--- backstage-app/examples/adnoc-template/skeleton/terraform/main.tf
+++ backstage-app/examples/adnoc-template/skeleton/terraform/main.tf
@@ -21,6 +21,9 @@ resource "azurerm_linux_web_app" "webapp" {
     # always_on requires Basic tier or higher (not compatible with Free/F1)
     always_on = "${{ values.sku }}" != "F1" ? true : false
     
+    # Startup command for Node.js apps
+    app_command_line = "npm start"
+    
     application_stack {
       # Dynamically sets runtime based on template selection (Node.js or .NET)
       node_version = split("|", "${{ values.runtime }}")[0] == "node" ? split("|", "${{ values.runtime }}")[1] : null
